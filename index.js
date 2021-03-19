762b38b19caf2cae07798936e4b4b49e48dfb647
Zhiming <i-jovial@hotmail.com>
2021-03-19 21:38:52 +0800

    Add API for retrieving top waypoints associated with STARs

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.get("/", (req, res) => {
   res.send("Express API server is up and running! Testing changes.");
 })
 
-app.get("/api/v1/busy-waypoints", (req, res) => {
+
+// SIDs
+app.get("/api/v1/sids/busy-waypoints", (req, res) => {
   const finalResultForJSON = [];
   
   var options = {
@@ -38,6 +40,82 @@ app.get("/api/v1/busy-waypoints", (req, res) => {
     // Part 2
     for (let airportName of airportMap.keys()) {
       options.uri = "https://open-atms.airlab.aero/api/v1/airac/sids/airport/" + airportName;
+      response = await rp(options);
+
+      if (response !== null && Object.keys(response).length > 0) {
+        // Count the number of waypoints associated with this airport (store as map)
+        const waypointMap = new Map();
+        for (let sid of response) {
+          for (let wp of sid.waypoints) {
+            if (!waypointMap.has(wp.name)) {
+              waypointMap.set(wp.name, 1);
+            } else {
+              waypointMap.set(wp.name, waypointMap.get(wp.name) + 1);
+            }
+          }
+        }
+
+        // Return a sorted array from the waypoints map
+        const sortedWaypointsArr = [...waypointMap.entries()].sort((a, b) => b[1] - a[1]);
+
+        // Extract out the top 2 waypoints
+        const tempArr = [];
+        for (var i = 0; i < 2; i++) {
+          if (sortedWaypointsArr[i].length > 1) {
+            const topWaypointsObj = {
+              name: sortedWaypointsArr[i][0],
+              count: sortedWaypointsArr[i][1],
+            }
+            tempArr.push(topWaypointsObj);
+          } else {
+            console.log("Unable to retrieve waypoint counts.");
+          }
+        }
+
+        const finalObj = {
+          airport: airportName,
+          topWaypoints: tempArr
+        }
+
+        finalResultForJSON.push(finalObj);
+      }
+    }
+
+    res.json(finalResultForJSON);
+  }
+
+  doRequests()
+  .catch(err => console.log(err));
+})
+
+// STARs
+app.get("/api/v1/stars/busy-waypoints", (req, res) => {
+  const finalResultForJSON = [];
+  
+  var options = {
+    uri: API_URL_AIRPORTS,
+    headers: {
+      'User-Agent': 'Request-Promise',
+      'api-key': API_KEY
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  async function doRequests() {
+    let response;
+
+    // Part 1
+    response = await rp(options);
+
+    const airportMap = new Map();
+    for (var airport of response) {
+      airportMap.set(airport.name, new Map());
+    }
+
+
+    // Part 2
+    for (let airportName of airportMap.keys()) {
+      options.uri = "https://open-atms.airlab.aero/api/v1/airac/stars/airport/" + airportName;
       response = await rp(options);
 
       if (response !== null && Object.keys(response).length > 0) {
